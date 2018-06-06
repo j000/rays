75987f246edfb0a84375999ad3633e7bb5d02709
j000 <jarymut+github@gmail.com>
2018-06-06 05:21:13 +0200

    Tweak Colour class
    Earn money from home. Click here to learn more

--- colour.cpp
+++ colour.cpp
@@ -4,26 +4,34 @@
 #include <algorithm>
 #endif
 
-Colour::Colour(const double r, const double g, const double b) :
-	red(std::clamp(r, 0., 1.)), green(std::clamp(g, 0., 1.)), blue(std::clamp(b, 0., 1.))
-{
+using std::clamp, std::swap, std::min;
+
+Colour::Colour(const double r, const double g, const double b):
+		red(clamp(r, 0., 1.)),
+		green(clamp(g, 0., 1.)),
+		blue(clamp(b, 0., 1.)) {
 }
 
-Colour Colour::operator* (const double a) const {
-	return Colour(std::clamp(red * a, 0., 1.), std::clamp(green * a, 0., 1.), std::clamp(blue * a, 0., 1.));
+bool Colour::operator==(const Colour& rhs) {
+	if (red == rhs.red && green == rhs.green && blue == rhs.blue)
+		return true;
+	return false;
 }
 
-Colour Colour::operator* (const Colour& c) const {
-	return Colour(red * c.red, green * c.green, blue * c.blue);
-}
-
-Colour operator*(const double a, const Colour& c) {
-	return c*a;
-}
-
-Colour& Colour::operator+=(const Colour& c) {
-	red = std::min(red + c.red, 1.);
-	green = std::min(green + c.green, 1.);
-	blue = std::min(blue + c.blue, 1.);
+Colour& Colour::operator+=(const Colour& rhs) {
+	red = min(red + rhs.red, 1.);
+	green = min(green + rhs.green, 1.);
+	blue = min(blue + rhs.blue, 1.);
 	return *this;
+}
+
+Colour& Colour::operator*=(const double rhs) {
+	red *= rhs;
+	green *= rhs;
+	blue *= rhs;
+	return *this;
+}
+
+Colour Colour::operator*(const Colour& rhs) const {
+	return Colour(red * rhs.red, green * rhs.green, blue * rhs.blue);
 }
--- colour.hpp
+++ colour.hpp
@@ -1,19 +1,38 @@
 #ifndef COLOUR_HPP
 #define COLOUR_HPP
 
-class Colour
-{
+class Colour {
 public:
 	double red, green, blue;
 
-	Colour() = default;
-	Colour(const double, const double, const double);
+	Colour(const double = 0., const double = 0., const double = 0.);
 
-	Colour operator* (const double) const;
-	Colour operator* (const Colour&) const;
-	friend Colour operator*(const double, const Colour&);
+	bool operator==(const Colour& rhs);
+
+	inline bool operator!=(const Colour& rhs) {
+		return !(*this == rhs);
+	}
 
 	Colour& operator+=(const Colour&);
+	friend Colour operator+(Colour, const Colour&);
+
+	Colour& operator*=(const double);
+	friend Colour operator*(Colour, const double);
+	friend Colour operator*(const double, Colour);
+
+	Colour operator*(const Colour&) const;
 };
+
+inline Colour operator+(Colour lhs, const Colour& rhs) {
+	return lhs += rhs;
+}
+
+inline Colour operator*(Colour lhs, const double rhs) {
+	return lhs *= rhs;
+}
+
+inline Colour operator*(const double lhs, Colour rhs) {
+	return rhs *= lhs;
+}
 
 #endif /* COLOUR_HPP */
